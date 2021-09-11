0224e79fa8b194d1a193a8cb130f2075555fcbd0
zelo78 <zelo78@yandex.ru>
2021-09-11 11:59:03 +0300

    first commit

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Product(models.Model):
+    '''Таблица "Товар"
+
+    id
+    Название товара
+    Ссылка на товар
+    Цена товара
+    Дата и время создания записи
+    '''
+    title = models.CharField(max_length=120)
+    link = models.URLField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_created=True)
+    # created_at - models.DateTimeField(auto_created=True)
+
+    def __str__(self):
+        return self.title
